c0ba5834dcc9039921750bf7b7048be0a2a22040
MFHKiwi <lauri22@online.de>
2021-04-02 12:59:57 +1300

    made pufferfish controllable

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    sf::Texture texture;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Super Smash ÆÜGH");
     window.setVerticalSyncEnabled(true);
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(30);
     window.clear(sf::Color::White);
+    sf::Texture texture;
     texture.loadFromFile("textures/pufferfish.png");
     texture.update(texture);
     texture.setSmooth(true);
     sf::Sprite sprite;
     sprite.setTexture(texture);
     sprite.setScale(0.5f, 0.5f);
-    sprite.setPosition(10.f, 10.f);
-    window.draw(sprite);
-    window.display();
+    float xpos = 30.f, ypos = 30.f;
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -28,6 +27,22 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		xpos = xpos - 7.f;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		xpos = xpos + 7.f;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                ypos = ypos - 7.f;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                ypos = ypos + 7.f;
+        }
+	sprite.setPosition(xpos, ypos);
+	window.clear(sf::Color::White);
+	window.draw(sprite);
+	window.display();
     }
 
     return 0;
