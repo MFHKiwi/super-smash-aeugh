fbf684d98c2bbe689ed805ccd0b0c944474b4080
MFHKiwi <lauri22@online.de>
2021-05-08 15:35:02 +1200

    fixed bug that caused lag issues due to srand() being spammed

--- src/randomV2f.hpp
+++ src/randomV2f.hpp
@@ -6,7 +6,9 @@
 using namespace sf;
 
 Vector2f randomV2f(CustomWindow& window) {
-	srand(time(NULL));
+	for (static bool first = true; first; first=false) {
+		srand(time(NULL));
+	}
 	int randomX = rand() % window.window.getSize().x;
 	int randomY = rand() % window.window.getSize().y;
 	Vector2f output(static_cast<float>(randomX), static_cast<float>(randomY));
@@ -14,8 +16,10 @@ Vector2f randomV2f(CustomWindow& window) {
 }
 
 Vector2f randomV2f(CustomWindow& window, CustomSprite& sprite) {
-        srand(time(NULL));
-        int randomX = rand() % window.window.getSize().x; // Generate random numbers.
+        for (static bool first = true; first; first=false) {
+                srand(time(NULL));
+        }
+	int randomX = rand() % window.window.getSize().x; // Generate random numbers.
         int randomY = rand() % window.window.getSize().y;
         randomX = (randomX > window.window.getSize().x / 2)? randomX : randomX + sprite.sprite.getGlobalBounds().width;
         randomY = (randomY > window.window.getSize().y / 2)? randomY - sprite.sprite.getGlobalBounds().height : randomY;
